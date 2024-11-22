d752bdb62fae46101632010c0a12e2dced4a6c74
Karina Vieira <karinamelov@gmail.com>
2024-11-22 15:31:23 -0300

    Other entities and second migration COM ERRO

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace SalesWebMvc.Models
 {
@@ -9,5 +9,27 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //Relação de 1 department para varios sellers. 1 para (lista de muitos)
+    
+        public Department()
+        {
+
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller (Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final)); //Busca o total de vendas de cada vendedor no periodo selecionado e soma o resultado para todos vendedores.
+
+        }
     }
 }
